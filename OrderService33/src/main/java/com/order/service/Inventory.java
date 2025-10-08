4131a7b035a23ceae647bb1b9e4aeebe6861cbd1
Omkar Mundhe <ommundhe67@gmail.com>
2025-10-08 11:17:40 +0530

    Kafka project created

---
+++ OrderService33/src/main/java/com/order/service/Inventory.java
@@ -0,0 +1,23 @@
+package com.order.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Inventory {
+    
+    private final List<String> messages = new ArrayList<>();
+
+    @KafkaListener(topics = "${topic.inventory}", groupId = "Inventory_group")
+    public void consume(String message) {
+        System.out.println("Received Order: " + message);
+        messages.add(message);
+    }
+
+    public List<String> getMessage() {
+        return messages;
+    }
+}
